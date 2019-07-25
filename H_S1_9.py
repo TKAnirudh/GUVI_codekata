4846f3cf214cd489451f6902e49cab6f55942aa1
TKA <katadepalli@gmail.com>
2019-07-25 20:54:53 +0530

    Update H_S1_9.py

--- H_S1_9.py
+++ H_S1_9.py
@@ -1,11 +1,26 @@
 n = input()
 x = list(map(int,input().split()))
+#x = list(dict.fromkeys(x))
 
+def mod(n):
+    if n<=0:
+        return -n
+    else:
+        return n
+y =[]
 for i in range(len(x)):
     for j in range(i+1,len(x)):
         #print (x[i],x[j])
-        if (i!=j) and (x[i]+x[j]==0):
-            print (x[i],x[j])
-            #continue
-
+        if (i!=j) and ((x[i]+x[j]==0)):
+            y.extend([x[i],x[j]])
+            print(x[i],x[j])
+            break
+            
+if len(y) == 0:
+    for i in range(len(x)):
+        for j in range(i+1,len(x)):
+            #print (x[i],x[j])
+            if (i!=j) and ((mod(x[i]+x[j])==1)):
+                print(x[i],x[j])
+                break
 
